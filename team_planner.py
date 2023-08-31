3c44404d93458915ee4e57340085d32ecdad1e52
osmed086 <OscarMederos@users.noreply.github.com>
2023-08-31 11:49:52 -0400

    Update team_planner.py
    Added error handling logic when taking input for base stat total to only accept integers.
    
    Modified the SQLite3 query to order the results by base stat total.

--- team_planner.py
+++ team_planner.py
@@ -1,13 +1,23 @@
 import sqlite3
 
+def get_valid_integer_input(prompt):
+    while True:
+        try:
+            value = int(input(prompt))
+            return value
+        except ValueError:
+            print("Please enter a valid integer.")
+
 conn = sqlite3.connect('pokemon.db')
 cursor = conn.cursor()
 
 print("Pokemon Team Builder 1.0")
 search_term_type = input("What type are you looking for? ")
-search_term_stats = input("What base stat total-minium are you looking for? ")
+search_term_stats = get_valid_integer_input("What base stat total minimum are you looking for? ")
 
-cursor.execute("SELECT * FROM pokedex WHERE type LIKE ? AND total > ?", (search_term_type, search_term_stats))
+search_term_type = '%' + search_term_type + '%'  # Wildcards for partial matches
+
+cursor.execute("SELECT * FROM pokedex WHERE LOWER(type) LIKE LOWER(?) AND total > ? ORDER BY total DESC", (search_term_type, search_term_stats))
 result = cursor.fetchall()
 conn.close()
 
